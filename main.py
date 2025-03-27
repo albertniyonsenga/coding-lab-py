82d106bba0588a1a7a9926e99fd5466fb079c28c
Albert NIYONSENGA <a.niyonseng@alustudent.com>
2025-03-27 09:17:20 +0000

    Main Logic addded
    After trying different way on my [Side GitHub account ](https://github.com/Al04ni/plagiarism-checker), we get simple logic to get the content from the file and then be able to check for plagiarism.

---
+++ main.py
@@ -0,0 +1,93 @@
+import os
+class plagiarism:
+    def into_set(self, doc):
+        if not isinstance(doc, str):
+            raise ValueError('supplied argument should be string')
+        set1 = {}
+        for words in doc.split():
+            set1.append(words)
+            
+    def check(self, set1, set2):
+        self.set1 = set(set1)
+        self.set2 = set(set2)
+        common = len(self.set1 & self.set2)
+        union = len(self.set1.union(self.set2))
+        formula = (common / union) * 100
+        
+        if formula >= 50:
+            print(f'''Alert:
+                The document contents are very similar with {formula}% which indicate plagiarism.
+                ''')
+        else:
+            print(f'''Caution:
+                The document contents are not similar with {formula}% which indicate no plagiarism.
+                ''')
+        
+    
+# Function to load the file
+def load_file(filename):
+    try:
+        with open(filename, 'r', encoding='utf-8') as f:
+            return f.read()
+        
+    except IOError as e:
+        print(f'Error reading file {filename}: {e}')
+        
+#Choosing file from the working directory
+def file_from_dir(dir='.'):
+    # List all the files in the dir
+    files = [f for f in os.listdir(dir) 
+            if os.path.isfile(os.path.join(dir, f)) and f.lower().endswith('.txt')]
+    if not files:
+        print('No text files found in director', dir)
+        return None
+    # Printing all files in dir
+    print('\nFiles found in Directory: ')
+    for idx, file in enumerate(files, start=1):
+        print(f'{idx}. {file}')
+        
+    while True:
+        try:
+            choice = int(input('Files You wanna select:'))
+            if 1 <= choice <= len(files):
+                selected = os.path.join(dir, files[choice-1])
+                print(f'Selected: {selected}')
+                return selected
+            else:
+                print('Please choose a nber btn 1 and ', len(files))
+                
+        except:
+            print('Invalid Input.Try again')
+
+#Checking the file
+def compare_text():
+    print('Select the First file')
+    file1 = file_from_dir()
+    if file1 is None:
+        return
+    print('\nSelect the Second file')
+    file2 = file_from_dir()
+    if file2 is None:
+        return
+    
+    vc = plagiarism()
+    con1 = vc.into_set(file1)
+    con2 = vc.into_set(file2)
+    similarity = vc.check(con1, con2)
+
+def menu():
+    while True:
+        print("\n--- Plagiarism Checker Menu ---")
+        print("1. Check Plagiarism(select via file list)")
+        print("2. Exit")
+        choice = input("Enter your choice : ").strip()
+        if choice == "1":
+            compare_text()
+        elif choice == "2":
+            print("Exiting the application.")
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
+if __name__ == "__main__":
+    menu()
